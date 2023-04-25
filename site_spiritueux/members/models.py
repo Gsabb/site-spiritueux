ab13334f7e4e74dc6314dc1d2124c2c1219b6118
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-04-25 18:09:29 -0400

    Fin templates, début banque de donnée (models)

--- site_spiritueux/members/models.py
+++ site_spiritueux/members/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Member(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    phone = models.IntegerField(null = True)
-    joined_date = models.DateField(null = True)
+class Client(models.Model):
+   user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True)
+   name = models.charField(max_length=200, null=True)
+   email = models.charField(max_length=200, null=True)
 
-    def __str__(self):
-        return f"{self.firstname} {self.lastname}"
+   def __str__(self):
+    return self.name
 
-class Cart(models.Model):
-    prix = models.IntegerField(null=True)
+class Produit(models.Model):
+    name = models.charField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default = False, null = True, blank = True)
+
+   def __str__(self):
+    return self.name
