a382ab13ace396990bd4aa349cff181cf9c6ce27
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-04-26 20:28:23 -0400

    panier et paiement dynamique

--- site_spiritueux/members/models.py
+++ site_spiritueux/members/models.py
@@ -27,11 +27,29 @@ class Commande(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.produitcommande_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.produitcommande_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
 class ProduitCommande(models.Model):
     product = models.ForeignKey(Produit, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Commande, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class AdresseLivraison(models.Model):
--- site_spiritueux/members/views.py
+++ site_spiritueux/members/views.py
@@ -3,11 +3,28 @@ from django.template import loader
 from .models import *
 
 def paiement(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.client
+        order, created = Commande.objects.get_or_create(client=customer, complete=False)
+        items = order.produitcommande_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'paiement.html', context)
 
 def panier(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.client
+        order, created = Commande.objects.get_or_create(client=customer, complete=False)
+        items = order.produitcommande_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'panier.html', context)
 
 def info(request):
