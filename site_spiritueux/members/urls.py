0a21d0811a5ccb18ea4c0d5114d26ab4e28464c8
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-01 16:43:37 -0400

    début logique panier, ajouter au cart

--- site_spiritueux/members/urls.py
+++ site_spiritueux/members/urls.py
@@ -5,8 +5,7 @@ urlpatterns = [
     path('', views.info, name='info'),
     path('boutique/', views.boutique, name='boutique'),
     path('panier/', views.panier, name='panier'),
-    path('paiement/', views.paiement, name='paiement')
-
-
+    path('paiement/', views.paiement, name='paiement'),
+    path('update_item/', views.updateItem, name="update_item")
     
 ]
--- site_spiritueux/members/views.py
+++ site_spiritueux/members/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from django.template import loader
 from .models import *
 
@@ -7,11 +9,13 @@ def paiement(request):
         customer = request.user.client
         order, created = Commande.objects.get_or_create(client=customer, complete=False)
         items = order.produitcommande_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'paiement.html', context)
 
 def panier(request):
@@ -20,11 +24,14 @@ def panier(request):
         customer = request.user.client
         order, created = Commande.objects.get_or_create(client=customer, complete=False)
         items = order.produitcommande_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'panier.html', context)
 
 def info(request):
@@ -32,6 +39,42 @@ def info(request):
     return render(request, 'info.html', context)
 
 def boutique(request):
+    if request.user.is_authenticated:
+        customer = request.user.client
+        order, created = Commande.objects.get_or_create(client=customer, complete=False)
+        items = order.produitcommande_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
     products = Produit.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems': cartItems}
     return render(request, 'boutique.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action: ',action)
+    print('Product ID: ', productId)
+
+    customer = request.user.client
+    product = Produit.objects.get(id = productId)
+    order, created = Commande.objects.get_or_create(client=customer, complete=False)
+    orderItem, created = ProduitCommande.objects.get_or_create(order = order, product = product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+
+    return JsonResponse('Item added', safe=False)
