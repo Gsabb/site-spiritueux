da8efa627e96375745c8082ce5e29cd0c714df99
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-03 18:45:50 -0400

    allow hosting

--- site_spiritueux/site_spiritueux/settings.py
+++ site_spiritueux/site_spiritueux/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxm_hxzxm6^!&5u#7!)@^au)dgahv$2*1eda$okez7bhnji0au'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://site-web-les-spiritueux.onrender.com']
 
 
 # Application definition
