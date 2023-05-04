7dda5d70ece1cf2d162c4de22f274c32b1ade626
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-03 22:04:41 -0400

    Update wsgi.py

--- site_spiritueux/site_spiritueux/wsgi.py
+++ site_spiritueux/site_spiritueux/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_spiritueux.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_spiritueux.site_spiritueux.settings')
 
 application = get_wsgi_application()
