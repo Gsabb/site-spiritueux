cd78478a6a8d46f15148ba6f57840ad141b401e8
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-03 22:40:09 -0400

    settings & urls

--- site_spiritueux/site_spiritueux/urls.py
+++ site_spiritueux/site_spiritueux/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('site_spiritueux.urls')),
+    path('', include('site_spiritueux/members.urls')),
     path('admin/', admin.site.urls),
 ]
 
