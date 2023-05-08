5317c3bed3caff8f8bee6c48089a1dd8203aa52d
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-08 13:19:04 -0400

    prepare static files

--- site_spiritueux/site_spiritueux/settings.py
+++ site_spiritueux/site_spiritueux/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'site_spiritueux.urls'
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'images/'
 
