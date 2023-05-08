55250c8120ea56044c1003bce193c6f005d0c0de
Guillaume Sabourin <123515500+Gsabb@users.noreply.github.com>
2023-05-08 13:54:05 -0400

    trying debug - boutique 500 error

--- site_spiritueux/site_spiritueux/settings.py
+++ site_spiritueux/site_spiritueux/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fxm_hxzxm6^!&5u#7!)@^au)dgahv$2*1eda$okez7bhnji0au'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['site-web-les-spiritueux.onrender.com']
 
